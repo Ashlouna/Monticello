f100df6751d2f2b142ed26c21e26c4e0a3a2af34
Oleksandr <ashlouna@gmail.com>
2021-06-18 14:27:17 +0300

    Rename marker file

--- js/main.js
+++ js/main.js
@@ -192,9 +192,9 @@ function initMap() {
   let marker = new google.maps.Marker({
     map: map,
     draggable: true,
-    animation: google.maps.Animation.DROP,
+    animation: google.maps.Animation.BOUNCE,
     position: { lat: 48.4153146, lng: 35.0694236 },
-    icon: "../img/Pin.png",
+    icon: "../img/pin.png",
   });
   marker.addListener("click", toggleBounce);
 
