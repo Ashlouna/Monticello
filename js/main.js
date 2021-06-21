11280846e9d4350be894e1e7f8387fd251b5213b
Oleksandr <ashlouna@gmail.com>
2021-06-21 14:21:23 +0300

    Added animation to marker

--- js/main.js
+++ js/main.js
@@ -1,15 +1,17 @@
 "use strict";
 
 const burger = document.querySelector("#burger");
-const headerNav = document.querySelector("#header-nav")
+const headerNav = document.querySelector("#header-nav");
 
 burger.onclick = () => {
   burger.classList.toggle("checked");
   headerNav.classList.toggle("mobile");
-}
+};
 
-let isEndAnimateLeft = true;
-let isEndAnimateRight = true;
+let isEndAnimateLeft = false;
+let isEndAnimateRight = false;
+let isMarkerOnMap = false;
+let map;
 
 window.addEventListener("scroll", () => {
   const weDoSection = document.querySelector("#projects");
@@ -22,77 +24,102 @@ window.addEventListener("scroll", () => {
   let articleCoordY = projectsArticle.getBoundingClientRect().y;
   let weDoCoordY = weDoSection.getBoundingClientRect().y;
 
-  if (isEndAnimateLeft && weDoCoordY < 80) {
+  if (!isEndAnimateLeft && weDoCoordY < 80) {
     projectImgLeft.classList.add("animate");
     setTimeout(() => {
       projectImgLeft.classList.remove("animate");
       projectImgLeft.classList.add("end-animate");
       fakeImgLeft.remove();
     }, 1500);
-    isEndAnimateLeft = false;
-  } else if(isEndAnimateRight && articleCoordY < 170) {
+    isEndAnimateLeft = true;
+  }
+
+  if (!isEndAnimateRight && articleCoordY < 170) {
     projectImgRight.classList.add("animate");
     setTimeout(() => {
       projectImgRight.classList.remove("animate");
       projectImgRight.classList.add("end-animate");
       fakeImgRight.remove();
     }, 2000);
-    isEndAnimateRight = false;
+    isEndAnimateRight = true;
   }
-})
 
+  const myMap = document.querySelector("#map");
+  let mapCoordY = myMap.getBoundingClientRect().y;
 
+  if (!isMarkerOnMap && mapCoordY < 160) {
+    let marker = new google.maps.Marker({
+      map: map,
+      draggable: true,
+      animation: google.maps.Animation.DROP,
+      position: { lat: 48.4153146, lng: 35.0694236 },
+      icon: "./img/pin.PNG",
+    });
+    const infowindow = new google.maps.InfoWindow({
+      content: "<h1>I am here!</h1>",
+    });
+    marker.addListener("click", toggleBounce);
+    marker.addListener("click", () => {
+      infowindow.open(map, marker);
+    });
+
+    function toggleBounce() {
+      if (marker.getAnimation() !== null) {
+        marker.setAnimation(null);
+      } else {
+        marker.setAnimation(google.maps.Animation.BOUNCE);
+      }
+    }
+
+    isMarkerOnMap = true;
+  }
+});
 
 $(document).ready(function () {
+  $(".slider").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: true,
+    adaptiveHeight: true,
+    autoplay: false,
+    centerPadding: "20px",
+  });
 
-    $('.slider').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: false,
-        dots: true,
-        adaptiveHeight: true,
-        autoplay: false,
-        centerPadding: "20px",
-    
-    })
-  
-  $('.news__slider').slick({
+  $(".news__slider").slick({
     slidesToShow: 3,
     slidesToScroll: 1,
     arrows: true,
     dots: true,
     autoplay: false,
     centerPadding: "20px",
-        responsive: [
-    {
-      breakpoint: 1210,
-      settings: {
-        arrows: true,
-        slidesToShow: 3
-      }
-    },
-    {
-      breakpoint: 1020,
-      settings: {
-        arrows: false,
-        slidesToShow: 2
-      }
-            },
-    {
-      breakpoint: 670,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: "0",
-        slidesToShow: 1
-      }
-    }
-        ],
+    responsive: [
+      {
+        breakpoint: 1210,
+        settings: {
+          arrows: true,
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 1020,
+        settings: {
+          arrows: false,
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 670,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: "0",
+          slidesToShow: 1,
+        },
+      },
+    ],
   });
-
 });
-
-
 
 let img = document.querySelectorAll(".gallery__img");
 let arr = [...img];
@@ -100,13 +127,11 @@ for (let item of arr) {
   item.onclick = changeImg;
 }
 // По клику меняет большую картинку в галерее
-function changeImg(){
+function changeImg() {
   let bigImg = document.querySelector("#big-img");
   let img = this.innerHTML;
   bigImg.innerHTML = img;
 }
-
-let map;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
@@ -193,33 +218,4 @@ function initMap() {
       },
     ],
   });
-
-  let marker = new google.maps.Marker({
-    map: map,
-    draggable: true,
-    animation: google.maps.Animation.BOUNCE,
-    position: { lat: 48.4153146, lng: 35.0694236 },
-    icon: "./img/pin.PNG",
-  });
-  // marker.addListener("click", toggleBounce);
-
-  const infowindow = new google.maps.InfoWindow({
-    content: "<h1>I am here!</h1>"
-  });
-
-  marker.addListener("click", () => {
-    infowindow.open(map, marker);
-    toggleBounce();
-  });
-
-  function toggleBounce() {
-    if (marker.getAnimation() !== null) {
-      marker.setAnimation(null);
-    } else {
-      marker.setAnimation(google.maps.Animation.BOUNCE);
-    }
-  }
 }
-
-
-
