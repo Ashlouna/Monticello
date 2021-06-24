e93d8ca62859ddc0f6f489f3b20fe89792a319a3
Oleksandr <ashlouna@gmail.com>
2021-06-24 17:07:10 +0300

    Added comments to main.js

--- js/main.js
+++ js/main.js
@@ -3,51 +3,53 @@
 const burger = document.querySelector("#burger");
 const headerNav = document.querySelector("#header-nav");
 
+// По клику на бургер меняем класс ему и меню
 burger.onclick = () => {
   burger.classList.toggle("checked");
   headerNav.classList.toggle("mobile");
 };
 
+// Флаг состояния анимации левой картинки в секции проектов
 let isEndAnimateLeft = false;
+// Флаг состояния анимации правой картинки в секции проектов
 let isEndAnimateRight = false;
+// Флаг состояния анимации маркера карты
 let isMarkerOnMap = false;
+// Карта
 let map;
 
+// Слушаем окно браузера во время скрола
 window.addEventListener("scroll", () => {
-  const weDoSection = document.querySelector("#projects");
-  const projectsArticle = document.querySelector("#projects-article");
   const projectImgLeft = document.querySelector("#project-img-left");
-  const projectImgRight = document.querySelector("#project-img-right");
+  const weDoSection = document.querySelector("#projects");
+  const weDoCoordY = weDoSection.getBoundingClientRect().y;
 
-  const fakeImgLeft = document.querySelector("#fake-img-left");
-  const fakeImgRight = document.querySelector("#fake-img-right");
-  let articleCoordY = projectsArticle.getBoundingClientRect().y;
-  let weDoCoordY = weDoSection.getBoundingClientRect().y;
-
+  // Если анимации ещё не было и проскролено столько
   if (!isEndAnimateLeft && weDoCoordY < 80) {
+    //добавить класс анимации
     projectImgLeft.classList.add("animate");
-    setTimeout(() => {
-      projectImgLeft.classList.remove("animate");
-      projectImgLeft.classList.add("end-animate");
-      fakeImgLeft.remove();
-    }, 1500);
+    //установить флаг, что анимация уже произошла и больше не нужна
     isEndAnimateLeft = true;
   }
 
+  const projectImgRight = document.querySelector("#project-img-right");
+  const projectsArticle = document.querySelector("#projects-article");
+  const articleCoordY = projectsArticle.getBoundingClientRect().y;
+
+  // Если анимации ещё не было и проскролено столько
   if (!isEndAnimateRight && articleCoordY < 170) {
+    //добавить класс анимации
     projectImgRight.classList.add("animate");
-    setTimeout(() => {
-      projectImgRight.classList.remove("animate");
-      projectImgRight.classList.add("end-animate");
-      fakeImgRight.remove();
-    }, 1500);
+    //установить флаг, что анимация уже произошла и больше не нужна
     isEndAnimateRight = true;
   }
 
   const myMap = document.querySelector("#map");
-  let mapCoordY = myMap.getBoundingClientRect().y;
+  const mapCoordY = myMap.getBoundingClientRect().y;
 
+  // Если анимации маркера ещё не было и проскролено столько
   if (!isMarkerOnMap && mapCoordY < 160) {
+    //Инициализируем маркер
     let marker = new google.maps.Marker({
       map: map,
       draggable: true,
@@ -55,14 +57,18 @@ window.addEventListener("scroll", () => {
       position: { lat: 48.4153146, lng: 35.0694236 },
       icon: "./img/pin.PNG",
     });
+    //Инициализируем инфо-сообщение всплывающее при клике на маркер
     const infowindow = new google.maps.InfoWindow({
       content: "<h1>I am here!</h1>",
     });
+    // По клику маркер начинает прыгать
     marker.addListener("click", toggleBounce);
+    // По клику всплывает сообщение
     marker.addListener("click", () => {
       infowindow.open(map, marker);
     });
 
+    // Функция прыгающего маркера
     function toggleBounce() {
       if (marker.getAnimation() !== null) {
         marker.setAnimation(null);
@@ -70,22 +76,21 @@ window.addEventListener("scroll", () => {
         marker.setAnimation(google.maps.Animation.BOUNCE);
       }
     }
-
+    //установить флаг, что анимация маркера уже произошла и больше не нужна
     isMarkerOnMap = true;
   }
 });
 
+// Слайдер в первой секции
 $(document).ready(function () {
   $(".slider").slick({
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows: false,
     dots: true,
-    adaptiveHeight: true,
-    autoplay: false,
-    centerPadding: "20px",
   });
 
+  //Слайдер новостей
   $(".news__slider").slick({
     slidesToShow: 3,
     slidesToScroll: 1,
@@ -93,7 +98,6 @@ $(document).ready(function () {
     dots: true,
     autoplay: true,
     autoplaySpeed: 4000,
-    centerPadding: "20px",
     responsive: [
       {
         breakpoint: 1210,
@@ -122,11 +126,15 @@ $(document).ready(function () {
   });
 });
 
+// Получаем коллекцию маленьких картинок в Gallery
 let img = document.querySelectorAll(".gallery__img");
+//Преобразуем коллекцию в массив
 let arr = [...img];
+//Вешаем на каждую картинку событие клик
 for (let item of arr) {
   item.onclick = changeImg;
 }
+
 // По клику меняет большую картинку в галерее
 function changeImg() {
   let bigImg = document.querySelector("#big-img");
@@ -134,6 +142,7 @@ function changeImg() {
   bigImg.innerHTML = img;
 }
 
+// Google map
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 48.4153146, lng: 35.0694236 },
